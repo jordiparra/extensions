ad1386b58438e7e6002fc2d1bbc535681ae22a79
Jordi Parra <hello@zenona.com>
2023-04-15 00:12:39 +0200

    Create translate-selection.ts

---
+++ extensions/google-translate/src/translate-selection.ts
@@ -0,0 +1,33 @@
+import { getSelectedText } from "@raycast/api";
+import translate from "@vitalets/google-translate-api";
+import { LanguageCode } from "./languages";
+
+export const AUTO_DETECT = "auto";
+
+export type SimpleTranslateResult = {
+  originalText: string;
+  translatedText: string;
+  pronunciation: string;
+  langFrom: LanguageCode;
+  langTo: LanguageCode;
+};
+
+export async function simpleTranslate(
+  options: { langFrom: LanguageCode; langTo: LanguageCode }
+): Promise<SimpleTranslateResult> {
+  const selectedText = await getSelectedText().catch(() => "");
+  const text = selectedText || "";
+  const isAutoDetect = options.langFrom === AUTO_DETECT;
+  const translated = await translate(text, {
+    from: isAutoDetect ? undefined : options.langFrom,
+    to: options.langTo,
+  });
+
+  return {
+    originalText: text,
+    translatedText: translated.text,
+    pronunciation: translated.pronunciation,
+    langFrom: isAutoDetect ? (translated?.from?.language?.iso as LanguageCode) : options.langFrom,
+    langTo: options.langTo,
+  };
+}
